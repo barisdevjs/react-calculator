f4585f60b1d8175739ccbc4ca218d11bc9fd88a8
Barış SAVAŞ <barissavas17@gmail.com>
2022-01-09 01:00:16 +0300

    new project

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="grid">
+    <div className="output">
+      <div className='prev'>wwwwww</div>
+      <div className='curr'>213232323</div>
+    </div>
+    <button className='span-two red'>AC</button>
+    <button className='gray'>DEL</button>
+    <button className='yellow'>/</button>
+    <button>7</button>
+    <button>8</button>
+    <button>9</button>
+    <button className='yellow'>x</button>
+    <button>4</button>
+    <button>5</button>
+    <button>6</button>
+    <button className='yellow'>-</button>
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button className='yellow'>+</button>
+    <button className='span-two'>0</button>
+    <button>.</button>
+    <button className='yellow'>=</button>
+
+
+
+
+    </div>
+    )
+}
+
+export default App;
